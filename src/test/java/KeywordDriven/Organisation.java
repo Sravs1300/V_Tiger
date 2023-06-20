4e38ad277faea58b028ab91b481734d5de3447f9
Sravs1300 <136672280+Sravs1300@users.noreply.github.com>
2023-06-20 15:01:02 +0530

    Update Organisation.java

--- src/test/java/KeywordDriven/Organisation.java
+++ src/test/java/KeywordDriven/Organisation.java
@@ -1,11 +1,31 @@
 package KeywordDriven;
 
-import org.openqa.selenium.WebDriver;
+import java.time.Duration;
 
-public class Organisation
-{
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Organisation {
+
 	public static void main(String[] args) {
+
+		//launching the browser
+		ChromeOptions co=new ChromeOptions();
+		co.addArguments("--remote-allow-origins=*");
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver(co);
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("http://localhost:8888/index.php");
 		
-	WebDriverManager.chr
+		//login operation 
+		
+		driver.findElement(By.name("user_name")).sendKeys("admin");
+		driver.findElement(By.name("user_password")).sendKeys("admin");	
+		driver.findElement(By.id("submitButton")).click();
 	}
 }
